9de87b6030f7300966817e595d5b1095209bc3e5
Azadulai <72621771+Azadulai@users.noreply.github.com>
2021-02-14 14:53:13 +0600

    true vs false

---
+++ true-fals.js
@@ -0,0 +1,23 @@
+// 0 
+// ""
+// false
+// undefined
+// null
+// NaN
+// sob guli false value...
+// sob guli true value...
+// '0'
+// ' '
+// []
+// {}
+// 'false'
+// const name = "0";
+let name = 0;
+// console.log(name);
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is fals");
+}
