778f274af58ca27799d156c4af5611db33588241
Azadulai <72621771+Azadulai@users.noreply.github.com>
2021-02-14 16:10:55 +0600

    null-undefinde done

---
+++ null-undefined.js
@@ -0,0 +1,18 @@
+let pakhi;
+function add(num1 ,num2){
+   console.log(num1 + num2); 
+   return
+}
+function add(num1, num2){
+    console.log(num2);
+}
+
+
+const premik = {name:"smart dude", phone:0125478};
+console.log(premik.gf);.
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [3,4,5,]
+console.log(ages[12]);
