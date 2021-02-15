d09938705ed8bd7ae2fd92e74522dff8a97dc388
Azadulai <72621771+Azadulai@users.noreply.github.com>
2021-02-15 12:52:33 +0600

    ok done

---
+++ scope.js
@@ -0,0 +1,22 @@
+
+let bonus =20;
+function sum(first, second){
+    let result = first + second +bonus;
+    //console.log(bonus);
+    if(result > 9){
+        var mood = "happy";
+        mood = "fun";
+        mood = "hasi";
+        mood = "khabi";
+        mood = "javi";
+        // console.log(mood);
+    }
+    console.log(mood);
+    var day = "friday"
+    return result;
+
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
